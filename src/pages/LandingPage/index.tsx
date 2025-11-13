872d199a2284dc5936cb9dbe7d3d3cca9b19a6cc
SPlinterRed <131634018+SPlinterRed@users.noreply.github.com>
2025-11-13 19:01:32 +0800

    fix .1

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/Hero";
 import Features from "../../components/Features";
 import Footer from "../../components/Footer";
+import { Header } from "@/components";
 
 const LandingPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-black">
+      <Header/>
       <Hero />
     </div>
   );
