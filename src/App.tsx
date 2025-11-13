471c311c61247db35e34968ce763db1ec2165c83
micomiconiiii <jhondelmico.abas@tup.edu.ph>
2025-11-13 18:50:14 +0800

    Refactoring UI-Mico

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,7 @@ export function App() {
 	return (
 		<>
 			<Header />
-		<main className="relative min-h-screen bg-black text-white overflow-hidden pt-28">
-			{/* 🔶 Background glow */}
-			<div className="absolute inset-0 bg-gradient-to-b from-[#FE9126]/25 via-[#050505]/25 to-transparent blur-1xl"></div>
-      		<div className="absolute top-1/4 left-1/2 -translate-x-1/2 w-[900px] h-[900px] bg-gradient-radial from-[#ff8a00]/25 to-transparent blur-[200px] rounded-full"></div>
+		<main className="relative min-h-screen bg-black text-white pt-28">
 			<div className="relative z-10 p-6 max-w-7xl mx-auto">
 				<Routes>
 					<Route path="/dashboard" element={<HomeDashboard />} />
